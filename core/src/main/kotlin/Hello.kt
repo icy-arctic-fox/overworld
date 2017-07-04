2dc68840408c236cfeea87a9fc4d08b800ab6d5f
Michael Miller <icy.arctic.fox@gmail.com>
2017-07-03 18:37:29 -0600

    Try bringing it together
    Exposed + DAO + H2

--- core/src/main/kotlin/Hello.kt
+++ core/src/main/kotlin/Hello.kt
@@ -1,3 +1,19 @@
+import com.turtleboxgames.overworld.entities.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.SchemaUtils.create
+
 fun foo(name: String) {
+    Database.connect("jdbc:h2:~/Desktop/h2test", driver = "org.h2.Driver")
     System.out.println("Hello, " + name)
+    transaction {
+        logger.addLogger(StdOutSqlLogger)
+        create(Games)
+        val game = Game.new {
+            this.name = "Test2"
+            description = "Foobar"
+        }
+        System.out.println(game.description)
+        commit()
+    }
 }
